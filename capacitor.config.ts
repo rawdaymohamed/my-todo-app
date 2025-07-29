20301587f7b72ce5e095b4527638e234327ffc89
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-07-29 05:47:17 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'My-todo-app',
+  webDir: 'dist'
+};
+
+export default config;
